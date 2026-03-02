2a9f45a966002663e0e775b9fd2e3266eaea53e7
sitiatiqa <>
2026-03-02 17:24:48 +0000

    Challenge: Manager-Employee Relationships

--- advanced_join_techniques_and_multi_table_queries/challenge_manager_employee_relationships/query.sql
+++ advanced_join_techniques_and_multi_table_queries/challenge_manager_employee_relationships/query.sql
@@ -1,1 +1,9 @@
 -- Write your code here
+SELECT 
+    e.name AS employee_name,
+    m.name AS manager_name
+    
+FROM 
+    employees e
+LEFT JOIN 
+    employees m ON m.employee_id = e.manager_id;
